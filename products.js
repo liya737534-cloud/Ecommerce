43a86be08a30e407a0be06f2d0de30074eb8ada0
liya737534-cloud <liya737534@gmail.com>
2026-04-22 01:29:01 -0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,26 @@
+
+async function getallproducts(){
+   var produts=await fetch("https://fakestoreapi.com/products",{
+    method:"GET"
+})
+
+var data=await produts.json()
+console.log(data)
+let productlist=""
+for(let p of data){
+    productlist=productlist+`<div class="card" style="width: 18rem;">
+  <img src="${p.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${p.title}</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-danger">Go somewhere</a>
+  </div>
+</div>`
+}
+
+ document.getElementById("productspace").innerHTML=productlist
+
+
+
+}
+getallproducts()
